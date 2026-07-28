137f944a3522d5df0d6534947626dcea2983fa5d
maxmullerbueno <max.albieri@outlook.com>
2026-07-28 07:21:37 +0000

    feat: comment as documentation to facilitate comprehension

--- headers/Player.h
+++ headers/Player.h
@@ -1,17 +1,18 @@
-#pragma once
-#include <string>
-#include <set>
+#pragma once                               // Prevents multiple inclusion of this header file
 
-class Player
+#include <string>                          // Provides the std::string class
+#include <set>                             // Provides the std:: set container
+
+class Player                               
 {
 private:
-      std::string name;
-      int attemptsRemaining;
-      std::set<char> guessedLetters;
+      std::string name;                    // Stores the player's name
+      int attemptsRemaining;               // Tracks the number of remaining attempts
+      std::set<char> guessedLetters;       // Stores unique letters guessed by the player
 
 public:
-      void addGuess(char guess);
-      bool hasGuessed(char guess);
-      void loseAttempt();
-      int getAttemptsRemaining();
+      void addGuess(char guess);           // Adds guessed letter to the player's collection
+      bool hasGuessed(char guess);         // Checks if the player has already guessed a letter
+      void loseAttempt();                  // Decreases the number of remaining attempts
+      int getAttemptsRemaining();          // Returns the number of attempts remaining
 }
