39bb57e41934ef1d2f88739b8531250f35a0f120
maxmullerbueno <max.albieri@outlook.com>
2026-07-29 01:07:51 +0000

    feat: private components designed in the UML and vector

--- headers/Country.h
+++ headers/Country.h
@@ -1,12 +1,15 @@
 #pragma once                               // Prevents multiple inclusion of this header file
 
 #include <string>                          // Provides the std::string class
+#include <vector>
 #include <set>                             // Provides the std:: set container
 
 class Country                               
 {
 private:
-
+      std::vector::std::string countries;
+      std::string selectedCountry;
+      std::set<char> discoveredLetters;
 public:
       std::string selectRandomCountry();
       bool::checkLetter(char);
