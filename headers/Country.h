962754624a8bafedade000b5bdf7a68e9acb9091
maxmullerbueno <max.albieri@outlook.com>
2026-07-29 01:17:37 +0000

    feat: Comments for documentation

--- headers/Country.h
+++ headers/Country.h
@@ -1,18 +1,18 @@
 #pragma once                               // Prevents multiple inclusion of this header file
 
 #include <string>                          // Provides the std::string class
-#include <vector>
+#include <vector>                          // Provides list of countries
 #include <set>                             // Provides the std:: set container
 
 class Country                               
 {
 private:
-      std::vector::std::string countries;
-      std::string selectedCountry;
-      std::set<char> discoveredLetters;
+      std::vector::std::string countries;  // List of available countries
+      std::string selectedCountry;         // Currently selected country
+      std::set<char> discoveredLetters;    // Letters correcntly guessed by the player
 public:
-      std::string selectRandomCountry();
-      bool::checkLetter(char);
-      bool::isComplete();
-      string::getDisplayWord();
+      std::string selectRandomCountry();   // Selects a random country
+      bool::checkLetter(char);             // Checks if a guessed letters exists
+      bool::isComplete();                  // Checks if the whole word has been discovered
+      string::getDisplayWord();            // Returns the word to display to the player
 }
