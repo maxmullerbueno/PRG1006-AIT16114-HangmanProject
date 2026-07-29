791f61ba2af9c4106fc38695b9d4b4362cb7171d
maxmullerbueno <max.albieri@outlook.com>
2026-07-29 15:00:47 +0000

    feat: ajustments at constructions adding srand

--- source/Country.cpp
+++ source/Country.cpp
@@ -5,10 +5,14 @@
 
 // Constructor countries
 Country::Country(){
+// Starts randomly based on current time
+srand(time(0));
+    
+// Provide list of countries
 countries = "Australia", "Brazil", "Canada", "Cambodia", "France", "China", "Indonesia", "Colombia", "Chile", "Bahamas", "Mexico", "Germany", 
     "Portugal", "Spain", "Uruguay", "Paraguay", "Korea", "Japan", "Russia", "South Africa", "United States", "New Zeland", "Greece", 
     "Turkey", "Thailand", "Philippines", "Panama", "England", "Chad", "Etiopia", "Egypt", "Morroco", "Italy", "Qatar", "Venezuela"
 }
 
-// Random countries selection
+// Pick it up one country of the list
 std::string Country::selectRandomCountry() {
