6089c2a9449d8f6dcbc00e4b4696d73cca09684f
maxmullerbueno <max.albieri@outlook.com>
2026-07-29 14:25:23 +0000

    feat: Addition of selected countries

--- source/Country.cpp
+++ source/Country.cpp
@@ -1,1 +1,10 @@
 #include "Country.h"                             // Country header
+
+// Selects a random country
+std::string Country::selectRandomCountry()
+{
+countries = {
+    "Australia", "Brazil", "Canada", "Cambodia", "France", "China", "Indonesia", "Colombia", "Chile", "Bahamas", "Mexico", "Germany", 
+    "Portugal", "Spain", "Uruguay", "Paraguay", "Korea", "Japan", "Russia", "South Africa", "United States", "New Zeland", "Greece", 
+    "Turkey", "Thailand", "Philippines", "Panama", "England", "Chad", "Etiopia", "Egypt", "Morroco", "Italy", "Qatar", "Venezuela"
+}
