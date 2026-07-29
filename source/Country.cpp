36aaa45e32a3152f5f3be1c2affa4091d1bb7d87
maxmullerbueno <max.albieri@outlook.com>
2026-07-29 14:50:09 +0000

    feat: Ajustment to Constructor and start of // Random countries selection

--- source/Country.cpp
+++ source/Country.cpp
@@ -1,10 +1,14 @@
 #include "Country.h"                             // Country header
 
-// Selects a random country
-std::string Country::selectRandomCountry()
-{
-countries = {
-    "Australia", "Brazil", "Canada", "Cambodia", "France", "China", "Indonesia", "Colombia", "Chile", "Bahamas", "Mexico", "Germany", 
+#include <cstdlib>                               // Necessary for rand() and srand()
+#include <ctime>                                 // Necessary for time()   
+
+// Constructor countries
+Country::Country(){
+countries = "Australia", "Brazil", "Canada", "Cambodia", "France", "China", "Indonesia", "Colombia", "Chile", "Bahamas", "Mexico", "Germany", 
     "Portugal", "Spain", "Uruguay", "Paraguay", "Korea", "Japan", "Russia", "South Africa", "United States", "New Zeland", "Greece", 
     "Turkey", "Thailand", "Philippines", "Panama", "England", "Chad", "Etiopia", "Egypt", "Morroco", "Italy", "Qatar", "Venezuela"
 }
+
+// Random countries selection
+std::string Country::selectRandomCountry() {
