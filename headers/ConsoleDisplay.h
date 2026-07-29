45635cbc94bad530eab122ad25132968bbb29b1f
maxmullerbueno <max.albieri@outlook.com>
2026-07-29 11:58:20 +0000

    feat: Adding includes from header

--- headers/ConsoleDisplay.h
+++ headers/ConsoleDisplay.h
@@ -1,11 +1,11 @@
 #pragma once                               // Prevents multiple inclusion of this header file
 
-class ConsoleDisplay         // Handles all console output for the game
+class ConsoleDisplay                       // Handles all console output for the game
 {
 public:
-void showWelcome();          // Displays welmcome message
-void showInstructions();     // Displays game instructions
-void showBoard();            // Displays game board
-void showResult();           // Display game result
-void drawHangman();          // Draws the Hangman figure
+void showWelcome();                        // Displays welmcome message
+void showInstructions();                   // Displays game instructions
+void showBoard();                          // Displays game board
+void showResult();                         // Display game result
+void drawHangman();                        // Draws the Hangman figure
 }
--- headers/HangmanGame.h
+++ headers/HangmanGame.h
@@ -1,12 +1,17 @@
 #pragma once                               // Prevents multiple inclusion of this header file
 
+#include <Country.h>                       // Country header
+#include <Player.h>                        // Player header
+#include <ConsoleDisplay.h>                // ConsoleDisplay header
+#include <Game.h>                          // Game header
+
 class HangmanGame
 {
 private:
 Player player;                             // HangmanGame class
 Country country;                           // Stores the player information
-ConsoleDisplay ConsoleDisplay;             // Stores the selected country      
- 
+ConsoleDisplay display;                    // Stores the selected country      
+
 public:
 void play();                               // Runs the game
 void startGame();                          // Starts the game
