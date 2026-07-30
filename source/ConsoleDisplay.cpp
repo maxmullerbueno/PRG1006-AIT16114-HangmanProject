73c18164ac9b0f732eabd56ce24fbe1b386c42b7
maxmullerbueno <max.albieri@outlook.com>
2026-07-30 05:51:47 +0000

    feat: Displays the game board

--- source/ConsoleDisplay.cpp
+++ source/ConsoleDisplay.cpp
@@ -1,36 +1,60 @@
 #include "ConsoleDisplay.h"          // ConsoleDisplay header
+using namespace std;
 
 #include <iostream>                  // Input and output
 
 // Displays the welcome screen
 void ConsoleDisplay::showWelcome()
 {
-    std::cout << std::endl;
-    std::cout << "     HANGMAN COUNTRIES       " << std::endl;
-    std::cout << "      GUESS THE WORLD!       " << std::endl;
-    std::cout << "-----------------------------" << std::endl;
-    std::cout << std::endl;
-    std::cout << "[1] Start Game" << std::endl;
-    std::cout << "[2] Instructions" << std::endl;
-    std::cout << "[1] Exit Game" << std::endl;
-    std::cout << std::endl;
-    std::cout << "-----------------------------" << std::endl;
-    std::cin << std::endl "Select your choice: " << std::endl;
+    cout << endl;
+    cout << "     HANGMAN COUNTRIES       " << endl;
+    cout << "      GUESS THE WORLD!       " << endl;
+    cout << "-----------------------------" << endl;
+    cout << endl;
+    cout << "[1] Start Game               " << endl;
+    cout << "[2] Instructions             " << endl;
+    cout << "[1] Exit Game                " << endl;
+    cout << endl;
+    cout << "-----------------------------" << endl;
+    cin >> endl "Select your choice:      " << endl;
 }
 
 // Displays the instructions
 void ConsoleDisplay::showInstructions()
 {
-    std::cout << "--------------------------------" << std::endl;
-    std::cout << "           HOW TO PLAY          " << std::endl;
-    std::cout << "--------------------------------" << std::endl;
-    std::cout << "> Guess the hidden country name " << std::endl;
-    std::cout << "> Enter one letter at a time    " << std::endl;
-    std::cout << "  or type the whole word        " << std::endl;
-    std::cout << "> You have 6 attempts           " << std::endl;
-    std::cout << "> Repeated guesses don't count  " << std::endl;
-    std::cout << "> Invalid input will be rejected" << std::endl;
-    std::cout << std::endl;
-    std::cout << "--------------------------------" << std::endl;
-    std::cout << "   Press ENTER to go back...    " << std::endl;
+    cout << "--------------------------------" << endl;
+    cout << "          HOW TO PLAY           " << endl;
+    cout << "--------------------------------" << endl;
+    cout << "> Guess the hidden country name " << endl;
+    cout << "> Enter one letter at a time    " << endl;
+    cout << "  or type the whole word        " << endl;
+    cout << "> You have 6 attempts           " << endl;
+    cout << "> Repeated guesses don't count  " << endl;
+    cout << "> Invalid input will be rejected" << endl;
+    cout << endl;
+    cout << "--------------------------------" << endl;
+    cout << "   Press ENTER to go back...    " << endl;
+}
+
+// Displays the game board
+void ConsoleDisplay::showBoard(string word, int attempts, string guessed)                 // String to determine word
+{
+    cout << "--------------------------------" << endl;
+    cout << "            HANGMAN             " << endl;
+    cout << "--------------------------------" << endl;
+
+    drawHangman();
+
+    cout << "--------------------------------" << endl;
+    cout << "Word: " << word << endl;                      // word determined at line 40
+    cout << "Attempts: " << endl;
+    for (int i = 0; i < attempts; i++)                     // Loop for attemps, int determined at line 40
+    {
+        cout << "[*]";
+    }
+    cout << endl;
+
+    cout << "Guessed: " guessed << endl;                   // guessed determined by string at line 40
+    cout << endl;
+    cout << "--------------------------------" << endl;
 }
