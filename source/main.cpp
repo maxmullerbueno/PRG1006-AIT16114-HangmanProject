fafd2c4aa4ea4d594283ef9065062657de7190c8
maxmullerbueno <max.albieri@outlook.com>
2026-07-30 07:39:12 +0000

    feat: Added int main() for testing Player, Country and ConsoleDisplay

--- source/main.cpp
+++ source/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>                                          // Includes Inputs and Outputs to the Library
+using namespace std;
 
 #include "ConsoleDisplay.h"                                  // ConsoleDisplay header
 #include "Country.h"                                         // Country header
@@ -6,3 +7,25 @@
 #include "HangmanGame.h"                                     // HangmanGame header
 #include "Player"                                            // Player header
 
+int main()
+{
+    Country country;
+    Player player;
+    ConsoleDisplay display;
+
+    display.showWelcome();
+    display.showInstructions();
+
+    display.showBoard(
+        country.getDisplayWord(),
+        player.getAttemptsRemaining(),
+    );
+
+    display.showResult(
+        true,
+        country.getCountryName(),
+        player.getAttemptsRemaining()
+    );
+
+    return 0;
+}
