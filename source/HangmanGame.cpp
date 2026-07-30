96db4b24408f29218f225aec8b48ba070adbbe42
maxmullerbueno <max.albieri@outlook.com>
2026-07-30 07:33:01 +0000

    feat: Adding showResult in HangmanGame.cpp

--- source/HangmanGame.cpp
+++ source/HangmanGame.cpp
@@ -1,1 +1,8 @@
 #include "HangmanGame.h"                        // HangamanGame header
+
+display.showResult(
+    true,
+    country.getCountryName(),
+    player.getAttemptsRemaining(),
+    player.getAttemptsRemaining()
+);
