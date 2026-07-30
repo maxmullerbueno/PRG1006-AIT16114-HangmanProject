2fe3701e3d8877509a61178c1c2706469f14df39
maxmullerbueno <max.albieri@outlook.com>
2026-07-30 04:44:18 +0000

    feat: adding showInstructions()

--- source/ConsoleDisplay.cpp
+++ source/ConsoleDisplay.cpp
@@ -17,3 +17,20 @@ void ConsoleDisplay::showWelcome()
     std::cout << "-----------------------------" << std::endl;
     std::cin << std::endl "Select your choice: " << std::endl;
 }
+
+// Displays the instructions
+void ConsoleDisplay::showInstructions()
+{
+    std::cout << "--------------------------------" << std::endl;
+    std::cout << "           HOW TO PLAY          " << std::endl;
+    std::cout << "--------------------------------" << std::endl;
+    std::cout << "> Guess the hidden country name " << std::endl;
+    std::cout << "> Enter one letter at a time    " << std::endl;
+    std::cout << "  or type the whole word        " << std::endl;
+    std::cout << "> You have 6 attempts           " << std::endl;
+    std::cout << "> Repeated guesses don't count  " << std::endl;
+    std::cout << "> Invalid input will be rejected" << std::endl;
+    std::cout << std::endl;
+    std::cout << "--------------------------------" << std::endl;
+    std::cout << "   Press ENTER to go back...    " << std::endl;
+}
