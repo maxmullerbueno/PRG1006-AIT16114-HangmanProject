60e7b14ed9c978e2a4517258dfa37d6e230037fb
maxmullerbueno <max.albieri@outlook.com>
2026-07-30 08:39:07 +0000

    feat: Added ; at the constructor

--- source/Country.cpp
+++ source/Country.cpp
@@ -1,21 +1,23 @@
 #include "Country.h"                             // Country header
+using namespace std;                             // To avoid std::
 
 #include <cstdlib>                               // Necessary for rand() and srand()
 #include <ctime>                                 // Necessary for time()   
 
 // Constructor countries
-Country::Country(){
+Country::Country()
+{
 // Starts randomly based on current time
 srand(time(0));
     
 // Provide list of countries
-countries = "Australia", "Brazil", "Canada", "Cambodia", "France", "China", "Indonesia", "Colombia", "Chile", "Bahamas", "Mexico", "Germany", 
+countries = {"Australia", "Brazil", "Canada", "Cambodia", "France", "China", "Indonesia", "Colombia", "Chile", "Bahamas", "Mexico", "Germany", 
     "Portugal", "Spain", "Uruguay", "Paraguay", "Korea", "Japan", "Russia", "South Africa", "United States", "New Zeland", "Greece", 
-    "Turkey", "Thailand", "Philippines", "Panama", "England", "Chad", "Etiopia", "Egypt", "Morroco", "Italy", "Qatar", "Venezuela"
+    "Turkey", "Thailand", "Philippines", "Panama", "England", "Chad", "Etiopia", "Egypt", "Morroco", "Italy", "Qatar", "Venezuela"};
 }
 
 // Pick it up one country of the list
-std::string Country::selectRandomCountry() {
+string Country::selectRandomCountry() {
     
     int index = rand() % countries.size();
     selectedCountry = countries[index];
