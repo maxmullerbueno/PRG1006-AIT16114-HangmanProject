f19db152cff16866b2c3151f7bae2a9382001501
maxmullerbueno <max.albieri@outlook.com>
2026-07-30 01:40:07 +0000

    feat: welcome screen to ConsoleDisplay.cpp

--- source/ConsoleDisplay.cpp
+++ source/ConsoleDisplay.cpp
@@ -1,1 +1,19 @@
-#include "ConsoleDisplay.h"                            // ConsoleDisplay header
+#include "ConsoleDisplay.h"          // ConsoleDisplay header
+
+#include <iostream>                  // Input and output
+
+// Displays the welcome screen
+void ConsoleDisplay::showWelcome()
+{
+    std::cout << std::endl;
+    std::cout << "     HANGMAN COUNTRIES       " << std::endl;
+    std::cout << "      GUESS THE WORLD!       " << std::endl;
+    std::cout << "-----------------------------" << std::endl;
+    std::cout << std::endl;
+    std::cout << "[1] Start Game" << std::endl;
+    std::cout << "[2] Instructions" << std::endl;
+    std::cout << "[1] Exit Game" << std::endl;
+    std::cout << std::endl;
+    std::cout << "-----------------------------" << std::endl;
+    std::cin << std::endl "Select your choice: " << std::endl;
+}
