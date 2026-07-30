8e990eef42818af03794c4273db66887b371f6e8
maxmullerbueno <max.albieri@outlook.com>
2026-07-30 13:53:58 +0000

    feat: Adding this->pointer to objects country and player

--- headers/ConsoleDisplay.h
+++ headers/ConsoleDisplay.h
@@ -8,6 +8,11 @@ Player* player;                                     // Pointer to the player
 
 public:
 ConsoleDisplay(Country* country, Player* player);   // Constructor: links the pointers to real objects
+{
+    this->country = country;                        // Links the country pointer to the actual Country object
+    this->player = player;                          // Links the player pointer to the actual Player object
+}
+
 
 void showWelcome();                                 // Displays welmcome message
 void showInstructions();                            // Displays game instructions
