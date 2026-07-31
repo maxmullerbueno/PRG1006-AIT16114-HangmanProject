d03af2a1aec1e3dc93ee2c3baf90acb681dbaae0
maxmullerbueno <max.albieri@outlook.com>
2026-07-31 10:18:44 +0000

    feat: Adding while loop

--- source/HangmanGame.cpp
+++ source/HangmanGame.cpp
@@ -6,30 +6,40 @@ HangmanGame::HangmanGame() : player(), country(), display(&country, &player){
 }
 
 // Plays the Hangman game
-void HangmanGame::play() {
+void HangmanGame::play() 
+{
+    bool userOption = true;
+
+    while (userOption)
+    {
+        
     int choice = display.showWelcome();
     
     // Switch statement for user's guidance
         switch (choice)
-    {
-        // Start Game
-        case 1:
         {
+            // Start Game
+            case 1:
+            {
             country.selectRandomCountry();
             display.showBoard();
             break;
-        }
-        // Instructions screen
-        case 2:
-        {
+            }
+
+            // Instructions screen
+            case 2:
+            {
             display.showInstructions();
             break;
-        }
-        // Exit game
-        case 3:
-        {
+            }
+            
+            // Exit game
+            case 3:
+            {
             exit();
+            userOption = false;
             break;
+            }
         }
     }
 }
