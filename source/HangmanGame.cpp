06b7e705efca5ff776aa7377d1800b5335c686cf
maxmullerbueno <max.albieri@outlook.com>
2026-07-31 09:53:32 +0000

    feat: adding switch and cases

--- source/HangmanGame.cpp
+++ source/HangmanGame.cpp
@@ -8,4 +8,18 @@ HangmanGame::HangmanGame() : player(), country(), display(&country, &player){
 // Plays the Hangman game
 void HangmanGame::play() {
     int choice = display.showWelcome();
+        switch (choice)
+    {
+        case 1:
+            // Start Game
+            break;
+
+        case 2:
+            // Instructions screen
+            break;
+
+        case 3:
+            // Exit game
+            break;
+    }
 }
