df8552a986febbfe346d6234fb58d2e658597767
maxmullerbueno <max.albieri@outlook.com>
2026-07-31 10:02:25 +0000

    feat: Adding commands for each case

--- source/HangmanGame.cpp
+++ source/HangmanGame.cpp
@@ -8,18 +8,28 @@ HangmanGame::HangmanGame() : player(), country(), display(&country, &player){
 // Plays the Hangman game
 void HangmanGame::play() {
     int choice = display.showWelcome();
+    
+    // Switch statement for user's guidance
         switch (choice)
     {
+        // Start Game
         case 1:
-            // Start Game
+        {
+            country.selectRandomCountry();
+            display.showBoard();
             break;
-
+        }
+        // Instructions screen
         case 2:
-            // Instructions screen
+        {
+            display.showInstructions();
             break;
-
+        }
+        // Exit game
         case 3:
-            // Exit game
+        {
+            exit();
             break;
+        }
     }
 }
