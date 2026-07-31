e9ee58f443eb0ca768d4e9dbfc9a3756ebe3c215
maxmullerbueno <max.albieri@outlook.com>
2026-07-31 09:25:03 +0000

    feat: return on main and HangmanGame::HangmanGame()

--- source/HangmanGame.cpp
+++ source/HangmanGame.cpp
@@ -1,2 +1,9 @@
 #include "HangmanGame.h"                        // HangamanGame header
 using namespace std;                            // To avoid std::
+
+HangmanGame::HangmanGame()
+    : player(),
+      country(),
+      display(&country, &player)
+{
+}
--- source/main.cpp
+++ source/main.cpp
@@ -12,4 +12,6 @@ int main() {
 {
     HangmanGame game;
     game.play();
+
+    return 0;
 }
