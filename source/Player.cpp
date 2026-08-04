4da012991e2de021f2f13e3d592bb09e3e17f286
maxmullerbueno <max.albieri@outlook.com>
2026-08-04 14:07:12 +0000

    feat: Changing guess[0] to (tolower(guess[0]) for upper and lowercase

--- source/Player.cpp
+++ source/Player.cpp
@@ -1,28 +1,39 @@
-#include "Player.h"
+#include "Player.h"                                                // Player header
+using namespace std;                                               // To avoid std::
+#include <cctype>                                                  // for tolower()
 
+// Implementation of the Player class methods
 void Player::addGuess(string guess)
 {
-    guessedLetters.insert(guess[0]);
+    guessedLetters.insert(tolower(guess[0]));
 }
 
+// Checks if player has already guessed a letter
 bool Player::hasGuessed(string guess)
 {
-    return guessedLetters.count(guess[0]) > 0;
+    return guessedLetters.count(tolower(guess[0])) > 0;
 }
 
+// Reduces number of attempts remaining by 1
 void Player::loseAttempt()
 {
     attemptsRemaining = attemptsRemaining - 1;
 }
 
+
+// Returns number of attempts remaining
 int Player::getAttemptsRemaining()
 {
     return attemptsRemaining;
 }
+
+// Returns letters guessed by player
 set<char> Player::getGuessedLetters() {
     return guessedLetters;
 }
-Player::Player(){                                              // Constructor implementation of number attempts remaining
+
+// Constructor implementation of number attempts remaining
+Player::Player(){                                    
     attemptsRemaining = 6;    
     guessedLetters.clear();                  
 }
