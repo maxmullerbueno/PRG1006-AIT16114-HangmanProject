bed0032d37e78fe83637559b3384cb1685a4b41f
maxmullerbueno <max.albieri@outlook.com>
2026-08-06 05:58:25 +0000

    feat: Adding ASCII to the other parts of the game messages

--- source/Country.cpp
+++ source/Country.cpp
@@ -14,7 +14,8 @@ srand(time(0));
 countries = {"Australia", "Brazil", "Canada", "Cambodia", "France", "China", "Indonesia", "Colombia", 
     "Chile", "Bahamas", "Mexico", "Germany", "Portugal", "Spain", "Uruguay", "Paraguay", "Korea", "Japan", 
     "Russia", "Greece", "Turkey", "Thailand", "Philippines", "Panama", "England", "Chad", "Ethiopia", 
-    "Egypt", "Morroco", "Italy", "Qatar", "Venezuela"};
+    "Egypt", "Morroco", "Italy", "Qatar", "Venezuela"
+};
 }
 
 // Pick it up one country of the list
