754c43a3fd57ec5b8fffd9776178ee94d52b102b
maxmullerbueno <max.albieri@outlook.com>
2026-08-06 10:52:16 +0000

    feat: Testing ASCII for each country at showResult

--- headers/ConsoleDisplay.h
+++ headers/ConsoleDisplay.h
@@ -11,7 +11,7 @@ Player* player;                                     // Pointer to the player
 public:
 ConsoleDisplay(Country* country, Player* player);   // Constructor: links the pointers to real objects
 
-int showWelcome();                                 // Displays welmcome message
+int showWelcome();                                  // Displays welmcome message
 void showInstructions();                            // Displays game instructions
 void showBoard();                                   // Displays game board
 void showResult();                                  // Display game result
