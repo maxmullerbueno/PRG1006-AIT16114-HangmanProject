e3fb6fcbe46ddede54982930b6843e6e35cff6ac
maxmullerbueno <max.albieri@outlook.com>
2026-08-07 14:44:48 +0000

    feat: Added comments as a complementation

--- headers/Country.h
+++ headers/Country.h
@@ -17,8 +17,8 @@ public:
 
       string selectRandomCountry();        // Selects a random country
       bool checkLetter(char);              // Checks if a guessed letters exists
-      bool isComplete() const;                   // Checks if the whole word has been discovered
-      string getDisplayWord() const;             // Returns the word to display to the player
-      string getSelectedCountry() const;         // Returns the selected country
+      bool isComplete() const;                   // Checks if the whole word has been discovered. Added const as remains the program unchanged.
+      string getDisplayWord() const;             // Returns the word to display to the player. Added const as remains the program unchanged.
+      string getSelectedCountry() const;         // Returns the selected country. Added const as remains the program unchanged.
       bool guessEntireWord(string guess);  // Guesses the whole word and checks if it is correct. Necessary as it shows in the Use Case Diagram
 };
--- headers/Player.h
+++ headers/Player.h
@@ -15,8 +15,8 @@ public:
       Player();                                           // Constructor declaration
 
       void addGuess(string guess);                        // Adds guessed letter to the player's collection
-      bool hasGuessed(const string& guess) const;         // Checks if the player has already guessed a letter
+      bool hasGuessed(const string& guess) const;         // Checks if the player has already guessed a letter. Added const as remains the program unchanged.
       void loseAttempt();                                 // Decreases the number of remaining attempts
-      int getAttemptsRemaining() const;                   // Returns the number of attempts remaining
-      const set<char> getGuessedLetters() const;                // Returns the letters guessed by the player
+      int getAttemptsRemaining() const;                   // Returns the number of attempts remaining. Added const as remains the program unchanged.
+      const set<char> getGuessedLetters() const;                // Returns the letters guessed by the player. Added const as remains the program unchanged.
 };
